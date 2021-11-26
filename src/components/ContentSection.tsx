63e70b3667551f400f5b501d66ee5568a7bad701
legitbeep <glorytoharsh@gmail.com>
2021-11-26 21:35:55 +0530

    feat: convert select, input to controlled

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useColorMode, Flex, Grid } from "@chakra-ui/react";
 import Image from "next/image";
 
@@ -8,6 +9,9 @@ import Card from "./ui/Card";
 const ContentSection = () => {
   const { colorMode } = useColorMode();
 
+  const [url, setUrl] = useState("");
+  const [format, setFormat] = useState("mp4");
+
   return (
     <Card>
       {/* <Flex gridGap={4}> */}
@@ -16,8 +20,16 @@ const ContentSection = () => {
           label="Youtube URL"
           name="link"
           placeholder="https://youtube.com/..."
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
         />
-        <CustomSelect label="Format" name="format" width="auto" />
+        <CustomSelect
+          label="Format"
+          name="format"
+          width="auto"
+          value={format}
+          onChange={(e) => setFormat(e.target.value)}
+        />
       </Grid>
       {/* </Flex> */}
     </Card>
