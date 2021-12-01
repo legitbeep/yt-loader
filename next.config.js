7a55e4e45888033b611d75508d169a9c73703663
legitbeep <glorytoharsh@gmail.com>
2021-12-01 16:58:24 +0530

    feat: add pwa

--- next.config.js
+++ next.config.js
@@ -5,10 +5,9 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     // disable: false,
-    disable:
-      process.env.NODE_ENV === "development" ||
-      process.env.NODE_ENV === "preview" ||
-      process.env.NODE_ENV === "production",
+    disable: process.env.NODE_ENV === "development", // ||
+    // process.env.NODE_ENV === "preview" ||
+    // process.env.NODE_ENV === "production",
     // delete two lines above to enable PWA in production deployment
     // add your own icons to public/manifest.json
     // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
